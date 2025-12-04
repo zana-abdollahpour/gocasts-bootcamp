98ad5d8e58d614af26353634357ef8f7ef10af63
zana-abdollahpour <zana.ap4@gmail.com>
2025-12-04 11:26:56 +0330

    01-two-fer

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func determineName(name string) string {
+	if name == "" {
+		return "you"
+	}
+	return name
+}
+
+func shutout(name string) {
+	fmt.Printf("one for %s, one for me\n", determineName(name))
+}
+
+func main() {
+	names := [4]string{"James", "Abraham", "", "Monica"}
+
+	for _, name := range names {
+		shutout(name)
+	}
 }
