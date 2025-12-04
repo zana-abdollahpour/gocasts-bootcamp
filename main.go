b8d9563560247cb1355d33c73d12ea21e414556b
zana-abdollahpour <zana.ap4@gmail.com>
2025-12-04 17:10:27 +0330

    08-scrabble-points

--- main.go
+++ main.go
@@ -1,44 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-type Clock struct {
-	hours   int
-	minutes int
+var points = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+	'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
 
-func NewClock(h, m int) Clock {
-	minutes := m % 60
+func calcScrabblePoint(word string) int {
+	score := 0
 
-	if minutes < 0 {
-		minutes += 60
-		h--
+	for _, letter := range strings.ToUpper(word) {
+		score += points[letter]
 	}
 
-	hours := (h + (m / 60)) % 24
-
-	if hours < 0 {
-		hours += 24
-	}
-
-	return Clock{
-		hours,
-		minutes,
-	}
-}
-
-func (c Clock) Add(m int) Clock {
-	return NewClock(c.hours, (c.minutes + m))
-}
-
-func (c Clock) Subtract(m int) Clock {
-	return NewClock(c.hours, (c.minutes - m))
-}
-
-func (c Clock) String() string {
-	return fmt.Sprintf("%.2d:%.2d", c.hours, c.minutes)
+	return score
 }
 
 func main() {
-	fmt.Println(NewClock(4, 44))
+	testCases := [3]string{"Giorno", "Lilith", "Jr.Zana"}
+
+	for _, word := range testCases {
+		fmt.Println(calcScrabblePoint(word))
+	}
 }
