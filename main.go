a00be07c11acc12644da3c7a5eabf49d2b74ecb0
zana-abdollahpour <zana.ap4@gmail.com>
2025-12-04 15:47:48 +0330

    06-luhn-algorithm

--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
-func calcDifferenceOfSquares(num int) int {
-	sum := 0
-	squareOfTheSum := 0
-	sumOfTheSquares := 0
+import "strings"
 
-	for i := range num + 1 {
-		if i == 0 {
-			continue
-		}
+func checkValidityViaLuhn(word string) bool {
+	num := strings.ReplaceAll(word, " ", "")
 
-		sum += i
-		sumOfTheSquares += i * i
+	if len(num) <= 1 {
+		return false
 	}
 
-	squareOfTheSum = sum * sum
+	lastIndex := len(num) - 1
+	sum := 0
 
-	return squareOfTheSum - sumOfTheSquares
+	for i, val := range num {
+		// Check if character is a digit
+		if val < '0' || val > '9' {
+			return false
+		}
+
+		// Convert rune to actual digit value
+		digit := int(val - '0')
+
+		// Double every second digit from the right
+		if (lastIndex-i)%2 != 0 {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+
+		sum += digit
+	}
+
+	return sum%10 == 0
 }
 
 func main() {
-	testcases := [4]int{10, 4, 14, 40}
+	testcases := [4]string{"4539 3195 0343 6467", "066 123 478"}
 
 	for i := range len(testcases) {
-		println(calcDifferenceOfSquares(testcases[i]))
+		println(checkValidityViaLuhn(testcases[i]))
 	}
 }
