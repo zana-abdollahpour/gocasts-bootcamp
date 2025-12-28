369b68e7ccac2770f44355d7844c7c6e0ddb4c08
zana-abdollahpour <zana.ap4@gmail.com>
2025-12-28 15:52:33 +0330

    implement unix which command replica

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	args := os.Args
+
+	var file string
+	if len(args) <= 1 {
+		fmt.Print("no file-name provided, please enter target filename: ")
+		fmt.Scan(&file)
+	} else {
+		file = args[1]
+	}
+
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+		if fileInfo, err := os.Stat(fullPath); err == nil {
+			mode := fileInfo.Mode()
+			if mode.IsRegular() && mode&0111 != 0 {
+				fmt.Println(fullPath)
+				return
+			}
+		}
+	}
+	fmt.Println("NOT FOUND!")
 }
